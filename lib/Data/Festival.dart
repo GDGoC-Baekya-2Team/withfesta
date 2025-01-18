f1f1e8ed2341095f4f374e232b2ce020170df7e8
uz <choyj0920@naver.com>
2025-01-19 00:19:28 +0900

    add LIB Table Calnendar

---
+++ lib/Data/Festival.dart
@@ -0,0 +1,14 @@
+class Festival {
+  Festival(this.festival_id,this.fstvlName,this.fstvlStart,this.fstvlEnd,this.number,this.location,this.mnnstNm,this.rdnmadr);
+
+  int festival_id=-1;
+  String? fstvlName="";
+  late DateTime? fstvlStart;
+  late DateTime? fstvlEnd;
+  String? number;
+  String? location;
+  String? mnnstNm;
+  String? rdnmadr;
+
+
+}
